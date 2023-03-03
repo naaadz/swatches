fc2b1e346ea78e1c01c59eaf56d7ad7bd282ee00
Nadia <naaadz@gmail.com>
2023-03-02 20:56:34 -0500

    setup tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue"
+import TheWelcome from "./components/TheWelcome.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	<header>
+		<img
+			alt="Vue logo"
+			class="logo"
+			src="./assets/logo.svg"
+			width="125"
+			height="125"
+		/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+		<div class="wrapper">
+			<HelloWorld msg="You did it!" />
+		</div>
+	</header>
 
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<TheWelcome />
+	</main>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
@@ -44,4 +50,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
